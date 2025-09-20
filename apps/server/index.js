bcd013fc510ef22e7bd3e2e4967e2effe0fa85e8
Xurvan <xurvan@tutanota.com>
2025-09-20 08:32:22 +0400

    feat: add client, server, and grape setup

---
+++ apps/server/index.js
@@ -0,0 +1,30 @@
+const { PeerRPCServer } = require("grenache-nodejs-ws");
+const Link = require("grenache-nodejs-link");
+
+function fibonacci(n) {
+	if (n <= 1) {
+		return 1;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const link = new Link({
+	grape: "http://127.0.0.1:30001",
+});
+link.start();
+
+const peer = new PeerRPCServer(link, {});
+peer.init();
+
+const service = peer.transport("server");
+service.listen(1337);
+
+setInterval(() => {
+	link.announce("fibonacci_worker", service.port, {});
+}, 1000);
+
+service.on("request", (_rid, _key, payload, handler) => {
+	const result = fibonacci(payload.number);
+	handler.reply(null, result);
+});
